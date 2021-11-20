df7885ba47f4132ced5fc257b85f1714b6949f00
Joonas Vanhalakka <joonas.vanhalakka@helsinki.fi>
2021-11-20 14:40:23 +0200

    uibuild

---
+++ build/precache-manifest.9020f8fe731b40e7561146476bb0696d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3df39967758a7faf3450ff34e1ca4c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25ee9fd4b9873ea818b9",
+    "url": "/static/js/2.90859df1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90859df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd6df1d528a6da5a8b55",
+    "url": "/static/js/main.3ba77040.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
